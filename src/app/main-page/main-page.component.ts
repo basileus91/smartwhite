842021b7778688eba2bc1f74cbbbeb6f9562506c
Vasile Furdui <vasiafurdui@yahoo.com>
2020-09-27 21:53:56 +0300

    making the slide presentation

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -4,7 +4,7 @@ import { HeaderLocalizationService } from '../shared/hader-localization.service'
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent implements OnInit {
   showSlide: number = 0;
@@ -15,13 +15,19 @@ export class MainPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.headerLocalizationService.blackHeader.next(true);
+    
   }
   
   displayOtherSlide(): void {
     console.log(this.showSlide);
-    if (this.showSlide >= 17) {
+    if (this.showSlide >= 31) {
       this.showSlide = 0;
     } else {
+      if (this.showSlide >= 11 && this.showSlide <= 21 || this.showSlide >= 30) {
+        this.headerLocalizationService.blackHeader.next(false);
+      } else {
+        this.headerLocalizationService.blackHeader.next(true);
+      }
       this.showSlide++;
     }
   }
