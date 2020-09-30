3d927824771ab1706dea5ccad169ec04e6dde0c2
Vasile Furdui <vasiafurdui@yahoo.com>
2020-09-30 20:54:52 +0300

    making the navbar responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthenticationService, User } from '../shared/auth.service';
@@ -7,7 +7,7 @@ import { HeaderLocalizationService } from '../shared/hader-localization.service'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css', './header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   closeResult: User;
@@ -18,13 +18,15 @@ export class HeaderComponent implements OnInit {
   isLogin: boolean = true;
   isBlackHeader: boolean = false;
   isCollapsed: boolean = false;
-  toggleNavbar:boolean = true;
+  isNavbarCollapsed: boolean;
 
   constructor(
     private readonly modalService: NgbModal,
     private readonly authService: AuthenticationService,
     private readonly headerLocalization: HeaderLocalizationService
-  ) { }
+  ) {
+    this.isNavbarCollapsed = true;
+   }
 
   ngOnInit(): void {
     this.authService.isLogged.subscribe(value => {
@@ -39,6 +41,7 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'md'}).result.then((result) => {
       this.closeResult = result;
@@ -50,4 +53,13 @@ export class HeaderComponent implements OnInit {
     this.authService.isLogged.next(false);
     this.authService.logout();
   }
+
+  collapseNavbar(): void {
+    this.isNavbarCollapsed = true;
+  }
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
 }
